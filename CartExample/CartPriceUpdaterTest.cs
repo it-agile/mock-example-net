7f65e28d33640a420242d4ea7eb3f731ce831542
Agile Developer ADS <csddev@it-agile.de>
2020-09-03 11:03:03 +0200

    Renaming

--- CartExample/CartPriceUpdaterTest.cs
+++ CartExample/CartPriceUpdaterTest.cs
@@ -8,29 +8,35 @@ namespace CartExample
     {
         Mock<CartArchive> archiveMock = new Mock<CartArchive>();
         Mock<PriceUpdates> priceMock = new Mock<PriceUpdates>();
+        CartPriceUpdater objectUnderTest;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            objectUnderTest = new CartPriceUpdater(archiveMock.Object, priceMock.Object);
+        }
 
         [TestMethod]
         public void TestCreateNewCartIfNotExists()
         {
-            CartPriceUpdater pc = new CartPriceUpdater(archiveMock.Object, null);
+            objectUnderTest = new CartPriceUpdater(archiveMock.Object, null);
             Cart newCart = new Cart();
 
             archiveMock.Setup(mc => mc.ById(0815)).Returns((Cart)null);
             archiveMock.Setup(mc => mc.CreateNewCart()).Returns(newCart);
 
-            Cart returnedCart = pc.RecalculateCart(0815);
+            Cart returnedCart = objectUnderTest.RecalculateCart(0815);
             Assert.AreEqual(newCart, returnedCart);
         }
 
         [TestMethod]
         public void TestEmptyCartNeverCallsPrice()
         {
-            CartPriceUpdater pc = new CartPriceUpdater(archiveMock.Object, priceMock.Object);
-            Cart newCart = new Cart().withId(1);
+            Cart cart = new Cart().withId(1);
 
-            archiveMock.Setup(mc => mc.ById(1)).Returns(newCart);
-            Cart returnedCart = pc.RecalculateCart(1);
-            Assert.AreEqual(newCart, returnedCart);
+            archiveMock.Setup(mc => mc.ById(1)).Returns(cart);
+            Cart returnedCart = objectUnderTest.RecalculateCart(1);
+            Assert.AreEqual(cart, returnedCart);
 
             priceMock.Verify(m => m.PriceForProduct(It.IsAny<string>()), Times.Never());
         }
@@ -38,25 +44,23 @@ namespace CartExample
         [TestMethod]
         public void TestCartWithOneItemIsUpdated()
         {
-            CartPriceUpdater pc = new CartPriceUpdater(archiveMock.Object, priceMock.Object);
-            Cart newCart = new Cart().withId(1).addProduct("Brot", 1.69);
+            Cart cart = new Cart().withId(1).addProduct("Brot", 1.69);
 
-            archiveMock.Setup(mc => mc.ById(1)).Returns(newCart);
+            archiveMock.Setup(mc => mc.ById(1)).Returns(cart);
             priceMock.Setup(mc => mc.PriceForProduct("Brot")).Returns(1.79);
-            Cart returnedCart = pc.RecalculateCart(1);
+            Cart returnedCart = objectUnderTest.RecalculateCart(1);
 
-            Assert.AreEqual(1.79, newCart.Items["Brot"]);
+            Assert.AreEqual(1.79, cart.Items["Brot"]);
         }
 
         [TestMethod]
         public void TestCartWithoutChangeDoesNotCallStats()
         {
-            CartPriceUpdater pc = new CartPriceUpdater(archiveMock.Object, priceMock.Object);
-            Cart newCart = new Cart().withId(1).addProduct("Brot", 1.69);
+            Cart cart = new Cart().withId(1).addProduct("Brot", 1.69);
 
-            archiveMock.Setup(mc => mc.ById(1)).Returns(newCart);
+            archiveMock.Setup(mc => mc.ById(1)).Returns(cart);
             priceMock.Setup(mc => mc.PriceForProduct("Brot")).Returns(1.69);
-            Cart returnedCart = pc.RecalculateCart(1);
+            Cart returnedCart = objectUnderTest.RecalculateCart(1);
 
             priceMock.Verify(m => m.PricesChanged(It.IsAny<int>()), Times.Never);
         }
@@ -64,12 +68,11 @@ namespace CartExample
         [TestMethod]
         public void TestCartWillCallStatistics()
         {
-            CartPriceUpdater pc = new CartPriceUpdater(archiveMock.Object, priceMock.Object);
-            Cart newCart = new Cart().withId(1).addProduct("Brot", 1.69);
+            Cart cart = new Cart().withId(1).addProduct("Brot", 1.69);
 
-            archiveMock.Setup(mc => mc.ById(1)).Returns(newCart);
+            archiveMock.Setup(mc => mc.ById(1)).Returns(cart);
             priceMock.Setup(mc => mc.PriceForProduct("Brot")).Returns(1.79);
-            Cart returnedCart = pc.RecalculateCart(1);
+            Cart returnedCart = objectUnderTest.RecalculateCart(1);
 
             priceMock.Verify(m => m.PricesChanged(1), Times.Once());
         }
@@ -77,22 +80,21 @@ namespace CartExample
         [TestMethod]
         public void TestCartCanHandleMultipleEntries()
         {
-            CartPriceUpdater pc = new CartPriceUpdater(archiveMock.Object, priceMock.Object);
-            Cart newCart = new Cart().withId(1)
+            Cart cart = new Cart().withId(1)
                 .addProduct("Brot", 1.69)
                 .addProduct("Butter", 1.19)
                 .addProduct("Marmelade", 2.69);
 
-            archiveMock.Setup(mc => mc.ById(1)).Returns(newCart);
+            archiveMock.Setup(mc => mc.ById(1)).Returns(cart);
             priceMock.Setup(mc => mc.PriceForProduct("Brot")).Returns(1.69);
             priceMock.Setup(mc => mc.PriceForProduct("Butter")).Returns(1.29);
             priceMock.Setup(mc => mc.PriceForProduct("Marmelade")).Returns(2.79);
-            Cart returnedCart = pc.RecalculateCart(1);
+            Cart returnedCart = objectUnderTest.RecalculateCart(1);
 
             priceMock.Verify(m => m.PricesChanged(2), Times.Once());
-            Assert.AreEqual(1.69, newCart.Items["Brot"]);
-            Assert.AreEqual(1.29, newCart.Items["Butter"]);
-            Assert.AreEqual(2.79, newCart.Items["Marmelade"]);
+            Assert.AreEqual(1.69, cart.Items["Brot"]);
+            Assert.AreEqual(1.29, cart.Items["Butter"]);
+            Assert.AreEqual(2.79, cart.Items["Marmelade"]);
         }
     }
 }
